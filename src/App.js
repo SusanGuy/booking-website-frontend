507e885b023ddd7df26c8792886b6c1fdb8fd814
gglee89 <gglee89@gmail.com>
2019-02-27 23:52:19 -0600

    CICD: Include gitlab config files.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+  };
+
+  componentDidMount() {
+    fetch('http://130.74.166.217:5000/user', {
+      method: 'GET',
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(result => {
+        this.setState({
+          users: result,
+        });
+      })
+      .catch(err => {
+        console.log('ERROR: ', err);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          {this.state.users.map(user => {
+            return (
+              <a
+                key={user.ID}
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {user.Name}
+              </a>
+            );
+          })}
         </header>
       </div>
     );
