f7a102dd6e74f83390b875c0d48a479542d80825
Hazelton Jacob <jhazelton11@gmail.com>
2019-03-03 15:56:05 -0600

    Login Responsive Fix & Hr Text

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -13,8 +13,7 @@ import ContentLayout from '../../layout/Content'
 
 const Div = styled.div`
   background-color: white;
-  height: 50%;
-  width: 50%;
+
   padding: 3vmin;
   display: flex;
   flex-direction: column;
@@ -23,17 +22,35 @@ const Div = styled.div`
 const Close = styled.span`
   background-color: white;
   color: black;
-
   margin: 20px 0 40px 0;
 `
-const HR = styled.hr`
-  margin: 20px 0 20px 0;
+
+const HRDivWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 `
+const HRDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`
+const HR = styled.hr`
+  width: 100%;
+`
+
+const HRTextDiv = styled.div`
+  margin: 1vmin;
+`
+
+const HRText = styled.p``
 
 const NAVLink = styled(NavLink)`
   color: black;
   padding: 10px;
 `
+
 const Login = () => {
   return (
     <ContentLayout>
@@ -45,23 +62,32 @@ const Login = () => {
           <Form.Group>
             <GoogleButton />
           </Form.Group>
-          <HR />
-          <Form.Group>
-            <Form.Control type='email' placeholder='Enter Email' />
-          </Form.Group>
-          <Form.Group>
-            <Form.Control type='password' placeholder='Password' />
-          </Form.Group>
-          <Form.Group>
-            <Button style={{ width: '100%', margin: '10px 0 10px 0' }}>
-              Login
-            </Button>
-          </Form.Group>
-          <Form.Group>
-            <Button style={{ width: '100%', margin: '10px 0 10px 0' }}>
-              Signup
-            </Button>
-          </Form.Group>
+          <HRDivWrap>
+            <HRDiv>
+              <HR />
+            </HRDiv>
+            <HRTextDiv>
+              <HRText>Or</HRText>
+            </HRTextDiv>
+            <HRDiv>
+              <HR />
+            </HRDiv>
+          </HRDivWrap>
+
+          <Form.Control type='email' placeholder='Enter Email' />
+
+          <Form.Control type='password' placeholder='Password' />
+
+          <Button
+            variant='primary'
+            style={{ width: '100%', margin: '10px 0 10px 0' }}
+          >
+            Login
+          </Button>
+
+          <Button style={{ width: '100%', margin: '10px 0 10px 0' }}>
+            Signup
+          </Button>
         </Form>
       </Div>
     </ContentLayout>
