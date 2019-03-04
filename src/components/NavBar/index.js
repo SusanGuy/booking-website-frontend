3e07a6325c8c01811210b104edd789dadaaf5f52
gglee89 <gglee89@gmail.com>
2019-03-04 02:13:51 -0600

    Component: Home component implemented.

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -11,76 +11,87 @@ import icons from '../../shared/icons';
 // ContextAPI
 import Consumer from '../../context/ConfigProvider';
 
-const NavBar = ({ windowWidth }) => {
-  console.log('windowWidth', windowWidth);
-  return (
-    <nav className="NavBar">
-      <div className="NavBar-wrapper">
-        <Consumer>
-          {({ mobileMenuExpanded, setMobileMenuExpanded }) => (
-            <React.Fragment>
-              {windowWidth <= 992 ? (
+class NavBar extends React.PureComponent {
+  render() {
+    const isMobileWidth = this.props.windowWidth <= 992;
+    return (
+      <nav className="NavBar">
+        <div className="NavBar-wrapper">
+          <Consumer>
+            {({ mobileMenuExpanded, setMobileMenuExpanded }) => (
+              <React.Fragment>
+                {isMobileWidth ? (
+                  <div
+                    onClick={() => setMobileMenuExpanded(!mobileMenuExpanded)}
+                    className="NavBar--logo"
+                  >
+                    <img src={icons.ecoHome} alt="logo-eco" />
+                    <i
+                      className={['material-icons']
+                        .concat(mobileMenuExpanded ? 'expanded' : '')
+                        .join(' ')}
+                    >
+                      expand_more
+                    </i>
+                  </div>
+                ) : (
+                  <Link to="/" className="NavBar--logo">
+                    <img src={icons.ecoHome} alt="logo-eco" />
+                  </Link>
+                )}
                 <div
-                  onClick={() => setMobileMenuExpanded(!mobileMenuExpanded)}
-                  className="NavBar--logo"
+                  className={[
+                    'NavBar--menu--mobile-bg',
+                    mobileMenuExpanded ? 'expanded' : '',
+                  ].join(' ')}
+                />
+                <ul
+                  className={[
+                    'NavBar--menu',
+                    mobileMenuExpanded ? 'expanded' : 'hide-on-med-and-down',
+                  ].join(' ')}
                 >
-                  <img src={icons.ecoHome} alt="logo-eco" />
-                  <i
-                    className={['material-icons']
-                      .concat(mobileMenuExpanded ? 'expanded' : '')
-                      .join(' ')}
-                  >
-                    expand_more
-                  </i>
-                </div>
-              ) : (
-                <Link to="/" className="NavBar--logo">
-                  <img src={icons.ecoHome} alt="logo-eco" />
-                </Link>
-              )}
-              <div
-                className={[
-                  'NavBar--menu--mobile-bg',
-                  mobileMenuExpanded ? 'expanded' : '',
-                ].join(' ')}
-              />
-              <ul
-                className={[
-                  'NavBar--menu',
-                  mobileMenuExpanded ? 'expanded' : 'hide-on-med-and-down',
-                ].join(' ')}
-              >
-                <li>
-                  <Link
-                    to="/property"
-                    onClick={() => setMobileMenuExpanded(!mobileMenuExpanded)}
-                  >
-                    Property
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/login"
-                    onClick={() => setMobileMenuExpanded(!mobileMenuExpanded)}
-                  >
-                    Login
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/about"
-                    onClick={() => setMobileMenuExpanded(!mobileMenuExpanded)}
-                  >
-                    About Us
-                  </Link>
-                </li>
-              </ul>
-            </React.Fragment>
-          )}
-        </Consumer>
-      </div>
-    </nav>
-  );
-};
+                  <li>
+                    <Link
+                      to="/property"
+                      onClick={() =>
+                        isMobileWidth &&
+                        setMobileMenuExpanded(!mobileMenuExpanded)
+                      }
+                    >
+                      Property
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/login"
+                      onClick={() =>
+                        isMobileWidth &&
+                        setMobileMenuExpanded(!mobileMenuExpanded)
+                      }
+                    >
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/about"
+                      onClick={() =>
+                        isMobileWidth &&
+                        setMobileMenuExpanded(!mobileMenuExpanded)
+                      }
+                    >
+                      About Us
+                    </Link>
+                  </li>
+                </ul>
+              </React.Fragment>
+            )}
+          </Consumer>
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default windowSize(NavBar);
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,7 +9,37 @@ import ContentLayout from '../../layout/Content';
 const Home = () => {
   return (
     <ContentLayout>
-      <div className="Home">Home</div>
+      <div className="Home">
+        <h1>
+          Plan your next{' '}
+          <span style={{ color: 'var(--theme-main)' }}>wildlife</span>{' '}
+          experience
+        </h1>
+        <div className="Home--search-location">
+          <div className="Home--fields col s12">
+            <div className="input-field" style={{ width: '48%' }}>
+              <i className="material-icons">search</i>
+              <input
+                placeholder="Fishing at Will Clay's sunny lake"
+                id="first_name"
+                type="text"
+                className="validate"
+              />
+            </div>
+            <div className="input-field with-label" style={{ width: '28%' }}>
+              <div className="label">DATES</div>
+              <div className="input-text">mm/dd/yyyy</div>
+            </div>
+            <div className="input-field with-label" style={{ width: '24%' }}>
+              <div className="label">GUESTS</div>
+              <div className="input-text">1 Guest</div>
+            </div>
+          </div>
+          <div className="button-field">
+            <i className="material-icons">search</i>
+          </div>
+        </div>
+      </div>
     </ContentLayout>
   );
 };
