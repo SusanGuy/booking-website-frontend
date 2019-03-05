46ae7248ee0af2aed6544abd865776509c26d698
gglee89 <gglee89@gmail.com>
2019-03-04 18:43:47 -0600

    Responsiveness: Include adaptive home layout.

--- src/layout/MenuContent/index.js
+++ src/layout/MenuContent/index.js
@@ -9,25 +9,16 @@ import backgrounds from '../../shared/backgrounds';
 // Components
 import NavBar from '../../components/NavBar';
 
-// ContextAPI
-import Consumer from '../../context/ConfigProvider';
-
 const MenuContent = ({ children }) => {
   return (
-    <Consumer>
-      {({ isMobileWidth }) => (
-        <div
-          className={['MenuContent', isMobileWidth ? 'collapsed' : ''].join(
-            ' '
-          )}
-          style={{ backgroundImage: `url(${backgrounds.landscape})` }}
-        >
-          <div className="MenuContent--hover-alpha" />
-          <NavBar />
-          {children}
-        </div>
-      )}
-    </Consumer>
+    <div
+      className="MenuContent"
+      style={{ backgroundImage: `url(${backgrounds.landscape})` }}
+    >
+      <div className="MenuContent--hover" />
+      <NavBar />
+      {children}
+    </div>
   );
 };
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DateRangePicker } from 'react-dates';
+import { SingleDatePicker, DateRangePicker } from 'react-dates';
 
 // Styles
 import './home.css';
@@ -9,6 +9,9 @@ import 'react-dates/lib/css/_datepicker.css';
 // Layout
 import ContentLayout from '../../layout/Content';
 
+// ContextAPI
+import Consumer from '../../context/ConfigProvider';
+
 class Home extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -17,6 +20,8 @@ class Home extends React.PureComponent {
       startDate: null,
       endDate: null,
       focusedInput: null,
+      startDateIsFocused: false,
+      endDateIsFocused: false,
       fieldFocusedInput: {
         location: false,
         date: false,
@@ -93,69 +98,118 @@ class Home extends React.PureComponent {
             <span style={{ color: 'var(--theme-main)' }}>wildlife</span>{' '}
             experience
           </h1>
-          <div className="Home--search-location">
-            <div className="Home--fields col s12">
-              <div
-                ref={input => (this.locationRef = input)}
-                className={[
-                  'input-field',
-                  'location',
-                  'with-label',
-                  location ? 'focused' : '',
-                ].join(' ')}
-                onClick={() => this.inputFocus('location')}
-              >
-                <div className="label">WHERE</div>
-                <input
-                  ref={input => (this.locationInputRef = input)}
-                  placeholder="Fishing at Will Clay's sunny lake"
-                  type="text"
-                  className="input-text"
-                />
+          <Consumer>
+            {({ isMobileWidth }) => (
+              <div className="Home--search-location">
+                <div className="Home--fields col s12">
+                  <div
+                    ref={input => (this.locationRef = input)}
+                    className={[
+                      'input-field',
+                      'location',
+                      'with-label',
+                      location ? 'focused' : '',
+                    ].join(' ')}
+                    onClick={() => this.inputFocus('location')}
+                  >
+                    <div className="label">WHERE</div>
+                    <input
+                      ref={input => (this.locationInputRef = input)}
+                      placeholder="Fishing at Will Clay's sunny lake"
+                      type="text"
+                      className="input-text"
+                    />
+                  </div>
+                  <div
+                    ref={dateRef => (this.dateRef = dateRef)}
+                    className={[
+                      'input-field',
+                      'date',
+                      'with-label',
+                      date ? 'focused' : '',
+                    ].join(' ')}
+                    onClick={() => this.inputFocus('date')}
+                  >
+                    {isMobileWidth ? (
+                      <div className="Home--SingleDatePicker">
+                        <div className="Home--DatePicker">
+                          <div className="label">CHECK IN</div>
+                          <SingleDatePicker
+                            date={this.state.startDate} // momentPropTypes.momentObj or null,
+                            placeholder="mm/dd/yyyy"
+                            id="start_date" // PropTypes.string.isRequired,
+                            onDateChange={date =>
+                              this.setState({ startDate: date })
+                            } // PropTypes.func.isRequired,
+                            numberOfMonths={1}
+                            onFocusChange={({ focused }) =>
+                              this.setState({ startDateIsFocused: focused })
+                            } // PropTypes.func.isRequired,
+                            focused={this.state.startDateIsFocused}
+                            withPortal={true}
+                            noBorder
+                          />
+                        </div>
+                        <div className="Home--DatePicker">
+                          <div className="label">CHECK OUT</div>
+                          <SingleDatePicker
+                            date={this.state.startDate} // momentPropTypes.momentObj or null,
+                            placeholder="mm/dd/yyyy"
+                            id="end_date" // PropTypes.string.isRequired,
+                            onDateChange={date =>
+                              this.setState({ endDate: date })
+                            } // PropTypes.func.isRequired,
+                            numberOfMonths={1}
+                            onFocusChange={({ focused }) =>
+                              this.setState({ endDateIsFocused: focused })
+                            } // PropTypes.func.isRequired,
+                            focused={this.state.endDateIsFocused}
+                            withPortal={true}
+                            noBorder
+                          />
+                        </div>
+                      </div>
+                    ) : (
+                      <React.Fragment>
+                        <div className="label">DATES</div>
+                        <DateRangePicker
+                          startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+                          startDatePlaceholderText="Check In"
+                          startDateId="start_date" // PropTypes.string.isRequired,
+                          endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+                          endDatePlaceholderText="Check Out"
+                          endDateId="end_date" // PropTypes.string.isRequired,
+                          onDatesChange={({ startDate, endDate }) =>
+                            this.setState({ startDate, endDate })
+                          } // PropTypes.func.isRequired,
+                          focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                          onFocusChange={focusedInput =>
+                            this.setState({ focusedInput })
+                          } // PropTypes.func.isRequired,
+                        />
+                      </React.Fragment>
+                    )}
+                  </div>
+                  <div
+                    ref={guestsRef => (this.guestsRef = guestsRef)}
+                    className={[
+                      'input-field',
+                      'guests',
+                      'with-label',
+                      guests ? 'focused' : '',
+                    ].join(' ')}
+                    onClick={() => this.inputFocus('guests')}
+                  >
+                    <div className="label">GUESTS</div>
+                    <div className="input-text">1 Guest</div>
+                  </div>
+                </div>
+                <div className="button-field">
+                  <i className="material-icons">search</i>
+                </div>
               </div>
-              <div
-                ref={dateRef => (this.dateRef = dateRef)}
-                className={[
-                  'input-field',
-                  'date',
-                  'with-label',
-                  date ? 'focused' : '',
-                ].join(' ')}
-                onClick={() => this.inputFocus('date')}
-              >
-                <div className="label">DATES</div>
-                <DateRangePicker
-                  startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-                  startDateId="start_date" // PropTypes.string.isRequired,
-                  endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-                  endDateId="end_date" // PropTypes.string.isRequired,
-                  onDatesChange={({ startDate, endDate }) =>
-                    this.setState({ startDate, endDate })
-                  } // PropTypes.func.isRequired,
-                  focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-                  onFocusChange={focusedInput =>
-                    this.setState({ focusedInput })
-                  } // PropTypes.func.isRequired,
-                />
-              </div>
-              <div
-                ref={guestsRef => (this.guestsRef = guestsRef)}
-                className={[
-                  'input-field',
-                  'guests',
-                  'with-label',
-                  guests ? 'focused' : '',
-                ].join(' ')}
-                onClick={() => this.inputFocus('guests')}
-              >
-                <div className="label">GUESTS</div>
-                <div className="input-text">1 Guest</div>
-              </div>
-            </div>
-            <div className="button-field">
-              <i className="material-icons">search</i>
-            </div>
-          </div>
+            )}
+          </Consumer>
         </div>
       </ContentLayout>
     );
