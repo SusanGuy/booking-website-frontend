dc4ab3fb723c9566e4f89414b50c77321cd7ce86
gglee89 <gglee89@gmail.com>
2019-03-05 22:05:42 -0600

    Include randomized top 8 results.

--- src/pages/Query/Wrapper.js
+++ src/pages/Query/Wrapper.js
@@ -7,12 +7,18 @@ import './wrapper.css';
 import ExperienceCard from './ExperienceCard';
 
 const Wrapper = ({ title, subtitle, experiences }) => {
+  // Pick ranndomized 4 options
+  const randomNumber = Math.floor(Math.random() * experiences.length - 4);
+  const randomizedExperiences = experiences
+    .slice(randomNumber, randomNumber + 8)
+    .map(experience => experience);
+
   return (
     <div className="Wrapper">
       <h3 className="Wrapper--title">{title}</h3>
       {subtitle && <h4 className="Wrapper--subtitle">{subtitle}</h4>}
       <div className="Wrapper--content">
-        {experiences.map((experience, key) => {
+        {randomizedExperiences.map((experience, key) => {
           return <ExperienceCard key={key} {...experience} />;
         })}
       </div>
